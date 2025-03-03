568a91566c45618738da501f5b62b7a46d6d7303
ValkyrieCain9 <kemymanyara@gmail.com>
2025-03-03 22:51:15 +0100

    regina added handler classes

---
+++ Handlers.py
@@ -0,0 +1,21 @@
+class Handler(object):
+    pass
+
+class UploadHandler(Handler):
+    pass
+
+class JournalUploadHandler(UploadHandler):
+    pass
+
+class CategoryUploadHandler(UploadHandler):
+    pass
+
+class QueryHandler(Handler):
+    pass
+
+class JournalQueryHandler(QueryHandler):
+    pass
+
+class CategoryQueryHandler(QueryHandler):
+    pass
+
