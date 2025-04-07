da367c9f0187e218c34d8f042c661a9d016fa40e
ValkyrieCain9 <kemymanyara@gmail.com>
2025-04-07 13:39:27 +0200

    modified graph upload handler

--- Handlers.py
+++ Handlers.py
@@ -24,8 +24,9 @@ class JournalUploadHandler(UploadHandler):
        #create graph to hold journal triples
        self.graph=rdf.Graph()
       
-       #add URI for attributes
+       #add URI for journal class
        self.Journal = "https://schema.org/Periodical"
+       #add URI for journal attributes
        self.id_issn = rdf.URIRef("https://schema.org/identifier/issn")
        self.id_eissn = rdf.URIRef("https://schema.org/identifier/eissn")
        self.title = rdf.URIRef("https://schema.org/name")
@@ -37,34 +38,53 @@ class JournalUploadHandler(UploadHandler):
 
        super().__init__(dbPathOrUrl)
 
+    def cleanData(self,df):
+        #Make column names shorter for readability 
+        df = df.rename(columns={'Journal ISSN (print version)':'Journal ISSN', 
+                                  'Journal EISSN (online version)':'Journal EISSN',
+                                  'Languages in which the journal accepts manuscripts':'Languages'})
+        #change null to ''
+        df = df.fillna('')
 
-    def addTriples(self):
-        # memo for later: if necessary, change non-values to empty strings, and define dtype
-        # check for values before making triples (if empty, what should we do?)
-        # check for one missing publisher
-        # check how to deal with languages        
-        journals = pd.read_csv(self.dbPathOrUrl)  
-        for idx, row in journals.iterrow():
+        #drop any row with absolutely no id 
+        df = df[df['Journal ISSN'] != '' & df['Journal EISSN']!= '']
+        
+        return True
+    
+    def addTriples(self,df):       
+        for idx, row in df.iterrow():
            local_id = "journal-" + str(idx)
            subj = rdf.URIRef(self.Journal + local_id)
-           self.graph.add((subj, self.id_issn, rdf.Literal["Journal ISSN (print version)"]))  
-           self.graph.add((subj, self.id_eissn, rdf.Literal["Journal EISSN (online version)"])) 
-           self.graph.add((subj, self.title, rdf.Literal["Journal title"])) 
-           self.graph.add((subj, self.language, rdf.Literal["Languages in which the journal accepts manuscripts"])) 
-           self.graph.add((subj, self.publisher, rdf.Literal["Publisher"])) 
-           self.graph.add((subj, self.seal, rdf.Literal["DOAJ Seal"])) 
-           self.graph.add((subj, self.license, rdf.Literal["Journal license"])) 
-           self.graph.add((subj, self.apc, rdf.Literal["APC"])) 
+           self.graph.add((subj, self.id_issn, rdf.Literal(row['Journal ISSN'])))  
+           self.graph.add((subj, self.id_eissn, rdf.Literal(row["Journal EISSN"])))
+           self.graph.add((subj, self.title, rdf.Literal(row["Journal title"])))
+
+           #Make seperate triples for each language
+           #self.graph.add((subj, self.language, rdf.Literal(row['Languages'])))
+           for lan in row['Languages'].split(', '):
+               self.graph.add((subj, self.language, rdf.Literal(lan)))
+
+           self.graph.add((subj, self.publisher, rdf.Literal(row["Publisher"])))
+           self.graph.add((subj, self.seal, rdf.Literal(row["DOAJ Seal"])))
+           self.graph.add((subj, self.license, rdf.Literal(row["Journal license"])))
+           self.graph.add((subj, self.apc, rdf.Literal(row["APC"])))
+        
         return True       
    
     def pushDataToDb(self, path):
+        df = pd.read_csv(path)
         store = rdf.SPARQLUpdateStore()
-        endpoint = "http://127.0.0.1:9999/blazegraph/sparql"
-        store.open((endpoint, endpoint))
+
+        self.cleanData(df)
+        self.addTriples(df)
+
+        store.open((self.dbPathOrUrl, self.dbPathOrUrl))
 
         for triple in self.graph.triples((None, None, None)):
            store.add(triple)
         store.close()
+
+        return True
 
 class CategoryUploadHandler(UploadHandler):
     pass
