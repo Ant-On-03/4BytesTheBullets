f68527e4ade681c4cb1837e042c647c1b68b216a
digitalctrlv <flinkert.a@hotmail.com>
2025-04-12 18:50:09 +0200

    message

--- Handlers.py
+++ Handlers.py
@@ -1,14 +1,51 @@
+import pandas as pd
+
 class Handler(object):
-    pass
+    def __init__(self, dbPathOrUrl):
+        self.dbPathOrUrl = dbPathOrUrl
+    
+    def getDbPathOrUrl(self):
+        return self.dbPathOrUrl
+
+    def setDbPathOrUrl(self, pathOrUrl):
+        self.dbPathOrUrl = pathOrUrl
+        return True
 
 class UploadHandler(Handler):
-    pass
+    def __init__(self, dbPathOrUrl):
+        super().__init__(dbPathOrUrl)
 
-class JournalUploadHandler(UploadHandler):
-    pass
+    def pushDataToDb(self, path):
+        pass
 
 class CategoryUploadHandler(UploadHandler):
-    pass
+    def __init__(self, dbPathOrUrl):
+        # Creating a relational database to host the tables
+
+        self.reldatabase = ...
+
+        super().__init__(dbPathOrUrl)
+
+    def addTables(self, df):
+        # here is where we add tables to the dataframe
+        return True
+    
+    def cleanData(self, df):
+        # the option to clean our data here
+        return True
+    
+    def pushDataToDb(self, path):
+        # this method is to load the json file into a dataframe that we will adjust using the method addTables
+        # and push to the database
+        df = pd.read_json(path)
+        # we need to see whether we want to clean our data
+        self.cleanData(df)
+        # this is where the dataframe created from the json file is added to our database 
+        # according to the method we formed addTables, specifying how many there should be
+        # and what names etc.
+        self.addTables(df)
+        
+        return True 
 
 class QueryHandler(Handler):
     pass
@@ -18,5 +55,3 @@ class JournalQueryHandler(QueryHandler):
 
 class CategoryQueryHandler(QueryHandler):
     pass
-
-#test
