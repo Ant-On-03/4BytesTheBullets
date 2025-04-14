f30dd5893eb79541c32790287efdfb2054e7ea0f
Antón Liñares López <antonlinares03@gmail.com>
2025-04-14 12:36:10 +0200

    Use of pointers

--- Handlers.py
+++ Handlers.py
@@ -1,4 +1,6 @@
+
 import pandas as pd
+from sqlite3 import connect
 
 class Handler(object):
     def __init__(self, dbPathOrUrl):
@@ -19,20 +21,104 @@ class UploadHandler(Handler):
         pass
 
 class CategoryUploadHandler(UploadHandler):
+
     def __init__(self, dbPathOrUrl):
-        # Creating a relational database to host the tables
-
-        self.reldatabase = ...
-
         super().__init__(dbPathOrUrl)
+        self.createTables()
+        
 
-    def addTables(self, df):
-        # here is where we add tables to the dataframe
+
+
+
+    def createTables(self):
+        # Creating a relational database to host the tables
+        connectionToDb =  connect(self.dbPathOrUrl)
+        cursorToDb = connectionToDb.cursor()
+
+        # Creating the tables
+        with cursorToDb:
+            # Creating the tables
+            cursorToDb.execute("""
+            CREATE TABLE Journals (
+                journal_id PRIMARY KEY,
+                issn UNIQUE
+            );
+
+            CREATE TABLE Categories (
+                category_id,
+                category_name UNIQUE,
+                quartile
+            );
+
+            CREATE TABLE Areas (
+                area_id INT AUTO_INCREMENT PRIMARY KEY,
+                area_name VARCHAR(255) UNIQUE
+            );
+
+            CREATE TABLE Journal_Categories (
+                journal_id INT,
+                category_id INT,
+                PRIMARY KEY (journal_id, category_id),
+                FOREIGN KEY (journal_id) REFERENCES Journals(journal_id),
+                FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+            );
+
+            CREATE TABLE Journal_Areas (
+                journal_id INT,
+                area_id INT,
+                PRIMARY KEY (journal_id, area_id),
+                FOREIGN KEY (journal_id) REFERENCES Journals(journal_id),
+                FOREIGN KEY (area_id) REFERENCES Areas(area_id)
+            );
+
+                           """)
+            
+
+    def pushDataToDb(self, filePath):
+        
+
+        # this method is to load the json file into a dataframe that we will adjust using the method addTables
+        # and push to the database
+        df = pd.read_json(filePath)
+        # we need to see whether we want to clean our data
+        self.cleanData(df)
+        # this is where the dataframe created from the json file is added to our database 
+        # according to the method we formed addTables, specifying how many there should be
+        # and what names etc.
+        self.addTables(df)
+
+
+        # we turn the json into python iterable dictionaries
+
+
+        #for journal in journals:
+
+            # save the journal id
+
+            # save the areas
+        #    for area in areas:
+                #save the areas
+
+
+
+        
         return True
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-    def cleanData(self, df):
-        # the option to clean our data here
-        return True
+
     
     def pushDataToDb(self, path):
         # this method is to load the json file into a dataframe that we will adjust using the method addTables
