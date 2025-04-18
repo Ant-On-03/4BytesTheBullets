978496612ac705f84806ca09981d33139b080893
digitalctrlv <flinkert.a@hotmail.com>
2025-04-18 13:53:50 +0200

    Handlers.py

--- Handlers.py
+++ Handlers.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+import json
 from sqlite3 import connect
 
 class Handler(object):
@@ -26,11 +27,64 @@ class CategoryUploadHandler(UploadHandler):
         super().__init__(dbPathOrUrl)
         self.createTables()
         
-
-
-
-
     def createTables(self):
+        # Creating the dataframes to be added to the database
+        df = pd.read_json(self.dbPathOrUrl)
+
+        ## --------------------------------------------------- JOURNAL DATAFRAME ------------------------------------------------------- ##
+
+        # We chose as the primary key the firs id (if its not None) or the second one (if the first is None)
+        # This is to avoid creating duplicate primary keys in the database when we supplementing the database with an additional dataset
+        df["unique_id"] = ["Journal-"+ row[0][0] if row[0][0] != None else "Journal-"+ row[0][1] for idx, row in df.iterrows()]
+
+        # We create a new dataframe with the unique_id and the identifiers columns
+        journals_df = df[["unique_id"]]
+        # we separate the ISSN and EISSN because the journals graph database sometimes does not have one of them.
+        journals_df[['ISSN', 'EISSN']] = pd.DataFrame(df['identifiers'].tolist(), index=df.index)
+
+        ## --------------------------------------------------- AREAS DATAFRAME --------------------------------------------------------- ##
+
+        # Get the Series 'areas' from the dataframe
+        areas_series = df["areas"]
+        # Use explode to spread multiple values in the areas columns across different rows
+        areas_series = areas_series.explode("areas")
+        unique_areas = areas_series.drop_duplicates()
+        # Convert this back to a dataframe to be added to the database
+        area_df = pd.DataFrame(unique_areas, columns=["areas"])
+
+        ## ---------------------------------------------- AREAS_JOURNALS DATAFRAME ---------------------------------------------------- ##
+
+        # We create a dataframe with the PRIMARY KEY for JOURNALS and for AREAS
+        areas_journals_dataframe = df[['unique_id', 'areas']]
+        # we SEPARATE the AREAS since there are many areas for each JOURNAL in the table.
+        areas_journals_dataframe = areas_journals_dataframe.explode("areas")
+
+        ## ------------------------------------------------ CATEGORIES DATAFRAME ------------------------------------------------------ ##
+
+        # we create a table with ALL the DICTIONATIES of the CATEGORIES
+        categories_dummy = df[['categories']].explode("categories")
+        # we normalize the dataframe into a flat table
+        categories_dataframe = pd.json_normalize(categories_dummy['categories'])
+
+        ## ------------------------------------------- JOURNALS_CATEGORIES DATAFRAME ------------------------------------------- ##
+
+        # Take the uniquely created identifiers from the journals dataframe
+        journals_categories_dataframe = pd.DataFrame(journals_df['unique_id'])
+        # Add the column from the original dataframe that still contains the list of dictionaries of 'ids' and 'quartiles'
+        journals_categories_dataframe.insert(1, "categories", df['categories'].values)
+        # Explode the list of dictionaries across multiple rows, so they correspond to the right journal as well
+        journals_categories_dataframe = journals_categories_dataframe.explode("categories")
+
+        # Extract id and quartile from each categories dictionary and use .apply() to add it as a string value to two new Series 
+        # 'id' and 'quartile' in the journals_category_dataframe
+        journals_categories_dataframe['id'] = journals_categories_dataframe['categories'].apply(lambda x: x['id'])
+        journals_categories_dataframe['quartile'] = journals_categories_dataframe['categories'].apply(lambda x: x['quartile'])
+
+        # Drop the original categories column that still holds the dictionaries
+        journals_categories_dataframe = journals_categories_dataframe.drop('categories', axis=1)
+
+
+        ## ------------------------------------------- POPULATING THE DATABASE ------------------------------------------- ##
         # Creating a relational database to host the tables
         connectionToDb =  connect(self.dbPathOrUrl)
         cursorToDb = connectionToDb.cursor()
@@ -56,7 +110,7 @@ class CategoryUploadHandler(UploadHandler):
                 area_name VARCHAR(255) UNIQUE
             );
 
-            CREATE TABLE Journal_Categories (
+            CREATE TABLE journal_Categories (
                 journal_id INT,
                 category_id INT,
                 PRIMARY KEY (journal_id, category_id),
@@ -64,7 +118,7 @@ class CategoryUploadHandler(UploadHandler):
                 FOREIGN KEY (category_id) REFERENCES Categories(category_id)
             );
 
-            CREATE TABLE Journal_Areas (
+            CREATE TABLE journal_Areas (
                 journal_id INT,
                 area_id INT,
                 PRIMARY KEY (journal_id, area_id),
@@ -73,11 +127,8 @@ class CategoryUploadHandler(UploadHandler):
             );
 
                            """)
-            
 
     def pushDataToDb(self, filePath):
-        
-
         # this method is to load the json file into a dataframe that we will adjust using the method addTables
         # and push to the database
         df = pd.read_json(filePath)
@@ -104,14 +155,9 @@ class CategoryUploadHandler(UploadHandler):
         return True
     
     def pushDataToDb(self, path):
-        # this method is to load the json file into a dataframe that we will adjust using the method addTables
-        # and push to the database
-        df = pd.read_json(path)
-        # we need to see whether we want to clean our data
+        
         self.cleanData(df)
-        # this is where the dataframe created from the json file is added to our database 
-        # according to the method we formed addTables, specifying how many there should be
-        # and what names etc.
+       
         self.addTables(df)
         
         return True 
--- test_JSON.py
+++ test_JSON.py
@@ -14,63 +14,81 @@ filePath = "./resources/scimago.json"
 
 df = pd.read_json(filePath)
 
-json = df[40:45]
-
-print(json)
-
+jsontest = df[:5].copy()
 
 # add indentifyers columns to the dataframe
 
 #just like this we have the 
-journals = json["identifiers"]
+journals = jsontest["identifiers"]
 
 ## ------------------------------------------- JOURNAL DATAFRAME ------------------------------------------- ##
 
 # We chose as the primary key the firs id (if its not None) or the second one (if the first is None)
 # This is to avoid creating duplicate primary keys in the database when we supplementing the database with an additional dataset
-json["unique_id"] = ["Journal-"+ row[0][0] if row[0][0] != None else "Journal-"+ row[0][1] for idx, row in json.iterrows()]
+jsontest["unique_id"] = ["Journal-"+ row[0][0] if row[0][0] != None else "Journal-"+ row[0][1] for idx, row in jsontest.iterrows()]
+
+################ERROR:#################
+# For line above
+# A value is trying to be set on a copy of a slice from a DataFrame.
+# Try using .loc[row_indexer,col_indexer] = value instead
+################ERROR:#################
 
 # We create a new dataframe with the unique_id and the identifiers columns
-journals_df = json[["unique_id"]]
+journals_df = jsontest[["unique_id"]]
 # we separate the ISSN and EISSN because the journals graph database sometimes does not have one of them.
-journals_df[['ISSN', 'EISSN']] = pd.DataFrame(json['identifiers'].tolist(), index=json.index)
+journals_df[['ISSN', 'EISSN']] = pd.DataFrame(jsontest['identifiers'].tolist(), index=jsontest.index)
 
-#print(journals_df)
-#print(json)
+################ERROR:#################
+# For line above
+# A value is trying to be set on a copy of a slice from a DataFrame.
+# Try using .loc[row_indexer,col_indexer] = value instead
+################ERROR:#################
 
 ## ------------------------------------------- AREAS DATAFRAME ------------------------------------------- ##
 
 
-areas_series = json[["areas"]]
+# Get the Series 'areas' from the dataframe
+areas_series = jsontest["areas"]
+# Use explode to spread multiple values in the areas columns across different rows
 areas_series = areas_series.explode("areas")
 unique_areas = areas_series.drop_duplicates()
+# Convert this back to a dataframe to be added to the database
+area_df = pd.DataFrame(unique_areas, columns=["areas"])
 
-
-# print(unique_areas)
-# eliminate duplicates
-#json = json.drop_duplicates(subset=['unique_id'])
+print(area_df)
 
 ## ------------------------------------------- AREAS_JOURNALS DATAFRAME ------------------------------------------- ##
 
 # We create a dataframe with the PRIMARY KEY for JOURNALS and for AREAS
-areas_journals_dataframe = json[['unique_id', 'areas']]
+areas_journals_dataframe = jsontest[['unique_id', 'areas']]
 # we SEPARATE the AREAS since there are many areas for each JOURNAL in the table.
 areas_journals_dataframe = areas_journals_dataframe.explode("areas")
 
 ## ------------------------------------------- CATEGORIES DATAFRAME ------------------------------------------- ##
 
 # we create a table with ALL the DICTIONATIES of the CATEGORIES
-dummy_dataframe = json[['categories']].explode("categories")
+dummy_dataframe = jsontest[['categories']].explode("categories")
 # we normalize the dataframe into a flat table
 categories_dataframe = pd.json_normalize(dummy_dataframe['categories'])
 
+# print(categories_dataframe)
 
 ## ------------------------------------------- JOURNALS_CATEGORIES DATAFRAME ------------------------------------------- ##
 
-print(json)
-journals_categories_dataframe = json[['unique_id', 'categories']]
-journals_categories_dataframe = journals_categories_dataframe.explode("categories")
-journals_categories_dataframe = pd.json_normalize(journals_categories_dataframe)
+# Take the unique identifiers from the journals dataframe
+journals_categories_dataframe = pd.DataFrame(journals_df['unique_id'])
 
-print(journals_categories_dataframe)
+
+journals_categories_dataframe.insert(1, "categories", jsontest['categories'].values)
+
+journals_categories_dataframe = journals_categories_dataframe.explode("categories")
+
+# Extract id and quartile from the categories dictionary
+journals_categories_dataframe['id'] = journals_categories_dataframe['categories'].apply(lambda x: x['id'])
+journals_categories_dataframe['quartile'] = journals_categories_dataframe['categories'].apply(lambda x: x['quartile'])
+
+# Drop the original categories column
+journals_categories_dataframe = journals_categories_dataframe.drop('categories', axis=1)
+
+# print(journals_categories_dataframe)
 
