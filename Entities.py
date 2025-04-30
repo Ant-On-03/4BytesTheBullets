32a23fbb597f97efc7f12c86bc0778be4aed86a7
ValkyrieCain9 <kemymanyara@gmail.com>
2025-04-30 15:44:22 +0200

    regina added entities classes and methods

--- Entities.py
+++ Entities.py
@@ -1,11 +1,62 @@
-class IdentifiableEnitity(object):
-    pass
+class IdentifiableEntity(object):
+    def __init__(self, id):
+        self.id = id
 
-class Journal(IdentifiableEnitity):
-    pass
+    def getIds(self):
+        return self.id
 
-class Category(IdentifiableEnitity):
-    pass
+class Journal(IdentifiableEntity):
+    def __init__(self, id, title, languages, seal, license, apc, publisher = None):
+        self.title = title
+        self.languages = languages
+        self.publisher = publisher
+        self.seal = seal
+        self.license = license
+        self.apc = apc
+        self.category = None
+        self.area = None
+        super().__init__(id)
 
-class Area(IdentifiableEnitity):
-    pass
+    def getTitle(self):
+        return self.title
+
+    def getLanguages(self):
+        return self.languages
+    
+    def getPublisher(self):
+        return self.publisher
+    
+    def hasDOAJSeal(self):
+        return self.seal
+
+    def getLicence(self):
+        return self.license
+    
+    def hasAPC(self):
+        return self.apc
+
+    def setCategories(self, categories):
+        self.category = categories
+        return True
+    
+    def getCategories(self):
+        return self.category
+
+    def setAreas(self, areas):
+        self.area = areas
+        return True 
+    
+    def getAreas(self):
+        return self.area
+
+class Category(IdentifiableEntity):
+    def __init__(self, id, quartile = None):
+        self.quartile = quartile
+        super().__init__(id)
+
+    def getQuartile(self):
+        return self.quartile
+
+class Area(IdentifiableEntity):
+    def __init__(self, id):
+        super().__init__(id)
