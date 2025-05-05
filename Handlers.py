23161f40817e11544c0f6f72b46dac2688c82f2e
ValkyrieCain9 <kemymanyara@gmail.com>
2025-05-05 18:25:01 +0200

    all handler classes and methods added

--- Handlers.py
+++ Handlers.py
@@ -1,8 +1,15 @@
+#libraries for graph database
 import rdflib as rdf
 import pandas as pd
 from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
 #Open in terminal: java -server -Xmx1g -jar blazegraph.jar
 from sparql_dataframe import get
+
+#libraries for relational database
+import pandas as pd
+from sqlite3 import connect
+import os
+from pandas import DataFrame
 
 class Handler(object):
     def __init__(self, dbPathOrUrl):
@@ -15,6 +22,8 @@ class Handler(object):
         self.dbPathOrUrl = pathOrUrl
         return True
 
+## ------------------------------------------- UPLOAD HANDLERS ------------------------------------------- ## 
+
 class UploadHandler(Handler):
     def __init__(self, dbPathOrUrl):
         super().__init__(dbPathOrUrl)
@@ -22,7 +31,7 @@ class UploadHandler(Handler):
     def pushDataToDb(self, path):
         pass
 
-class JournalUploadHandler(UploadHandler):
+class JournalUploadHandler(UploadHandler): #Shiho and Regina
     def __init__(self, dbPathOrUrl):
        #create graph to hold journal triples
        self.graph=rdf.Graph(identifier=rdf.URIRef('https://github.com/Ant-On-03/4BytesTheBullets/DOAJGraph'))
@@ -130,8 +139,173 @@ class JournalUploadHandler(UploadHandler):
         return True
 
     
-class CategoryUploadHandler(UploadHandler):
-    pass
+class CategoryUploadHandler(UploadHandler): #Anton and Anouk
+
+    def __init__(self, dbPathOrUrl):
+        super().__init__(dbPathOrUrl)
+        self.createTables()
+        
+    def createTables(self):
+        ## ------------------------------------------- CREATING THE DATABASE ------------------------------------------- ##
+        # Creating a relational database to host the tables
+
+        if os.path.exists(self.dbPathOrUrl):
+            os.remove(self.dbPathOrUrl)
+
+        connectionToDb =  connect(self.dbPathOrUrl)
+        cursorToDb = connectionToDb.cursor()
+
+        # Creating the tables
+
+        cursorToDb.execute("""
+                            
+        CREATE TABLE journals (
+            journal_id TEXT PRIMARY KEY,
+            ISSN TEXT,
+            EISSN TEXT
+        );""")
+
+        cursorToDb.execute("""
+                            
+        CREATE TABLE areas (
+            area_id TEXT PRIMARY KEY
+        );
+        """)
+
+        # FOR US CATEGORIES IS A WEAK ENTITY, THAT DEPENDS ON THE JOURNAL ID.
+        cursorToDb.execute("""
+                            
+        CREATE TABLE categories (
+            journal_id TEXT,
+            category_id TEXT,
+            quartile TEXT,
+            PRIMARY KEY (journal_id, category_id),
+            FOREIGN KEY (journal_id) REFERENCES journals(journal_id)
+        );
+
+        """)
+        cursorToDb.execute("""
+                            
+        CREATE TABLE areas_journals (
+            journal_id TEXT,
+            area_id TEXT,
+            PRIMARY KEY (journal_id, area_id),
+            FOREIGN KEY (journal_id) REFERENCES journals(journal_id),
+            FOREIGN KEY (area_id) REFERENCES areas(area_id)
+        );
+
+                        """) 
+
+        # Commit the changes and close the connection
+        connectionToDb.commit()
+        connectionToDb.close()
+
+        return True
+        
+    def pushDataToDb(self, filePath):
+
+        # Creating the dataframes to be added to the database
+        df = pd.read_json(filePath)
+    
+        ## ------------------------------------------- JOURNAL DATAFRAME ------------------------------------------- ##
+
+        # We chose as the primary key the firs id (if its not None) or the second one (if the first is None)
+        # This is to avoid creating duplicate primary keys in the database when we supplementing the database with an additional dataset
+        df["journal_id"] = ["Journal-"+ row[0][0] if row[0][0] != None else "Journal-"+ row[0][1] for idx, row in df.iterrows()]
+
+        ### WE DROP THE DUPLICATES
+        df = df.drop_duplicates(subset=["journal_id"])
+
+
+        # We create a new dataframe with the unique_id and the identifiers columns
+        journals_df = df[["journal_id"]]
+        # we separate the ISSN and EISSN because the journals graph database sometimes does not have one of them.
+        journals_df[['ISSN', 'EISSN']] = pd.DataFrame(df['identifiers'].tolist(), index=df.index)
+
+        ## ------------------------------------------- AREAS DATAFRAME ------------------------------------------- ##
+        # Get the Series 'areas' from the dataframe
+        areas_series = df["areas"]
+        # Use explode to spread multiple values in the areas columns across different rows
+        areas_series = areas_series.explode("areas")
+        unique_areas = areas_series.drop_duplicates()
+        # Convert this back to a dataframe to be added to the database
+        area_df = pd.DataFrame(unique_areas, columns=["areas"])
+        area_df.rename(columns={"areas": "area_id"}, inplace=True)
+
+        ## ------------------------------------------- AREAS_JOURNALS DATAFRAME ------------------------------------------- ##
+
+        # We create a dataframe with the PRIMARY KEY for JOURNALS and for AREAS
+        areas_journals_dataframe = df[['journal_id', 'areas']]
+        # we SEPARATE the AREAS since there are many areas for each JOURNAL in the table.
+        areas_journals_dataframe = areas_journals_dataframe.explode("areas")
+        areas_journals_dataframe.rename(columns={"areas": "area_id"}, inplace=True)
+
+        ## ------------------------------------------- CATEGORIES DATAFRAME ------------------------------------------- ##
+
+        # Take the unique identifiers from the journals dataframe
+        categories_dataframe = pd.DataFrame(journals_df['journal_id'])
+        categories_dataframe.insert(1, "categories", df['categories'].values)
+
+        categories_dataframe = categories_dataframe.explode("categories")
+        # Extract id and quartile from the categories dictionary
+        categories_dataframe['category_id'] = categories_dataframe['categories'].apply(lambda x: x['id'])
+        categories_dataframe['quartile'] = categories_dataframe['categories'].apply(lambda x: x.get('quartile'))
+        # Drop the original categories column
+        categories_dataframe = categories_dataframe.drop('categories', axis=1)
+
+
+        ############## ----------------- INSERTING DATA INTO THE DATABASE ----------------- ##############
+        # Creating the connection to the database
+        connectionToDb = connect(self.dbPathOrUrl)
+        cursorToDb = connectionToDb.cursor()
+
+        # Iterating through the dataframes and inserting the data into the database
+        for idx, row in journals_df.iterrows():
+            cursorToDb.execute("""
+                               INSERT INTO journals (journal_id, ISSN, EISSN) 
+                               VALUES (?, ?, ?);
+                               """, (row["journal_id"], row["ISSN"], row["EISSN"]))
+            
+        for idx, row in area_df.iterrows():
+            cursorToDb.execute("""
+                               INSERT INTO areas (area_id) 
+                               VALUES (?);
+                               """, (row["area_id"],))
+        
+        for idx, row in areas_journals_dataframe.iterrows():
+            cursorToDb.execute("""
+                               INSERT INTO areas_journals (journal_id, area_id) 
+                               VALUES (?, ?);
+                               """, (row["journal_id"], row["area_id"]))
+        
+        for idx, row in categories_dataframe.iterrows():
+            cursorToDb.execute("""
+                               INSERT INTO categories (journal_id, category_id, quartile)
+                               VALUES (?, ?, ?);
+                               """, (row["journal_id"], row["category_id"], row["quartile"]))
+        
+        # Commit the changes and close the connection
+        connectionToDb.commit()
+        connectionToDb.close()
+
+        return True
+    
+    def __str__(self):
+
+         # return all the categories in a database, with no repetitions.
+
+        conn = connect(self.dbPathOrUrl)
+        cursor = conn.cursor()
+        cursor.execute("SELECT category_id FROM categories;")
+        
+        categories = cursor.fetchall()
+        df = pd.DataFrame(categories, columns=["category_id"])
+
+        conn.close()
+    
+        return df
+
+## ------------------------------------------- QUERY HANDLERS ------------------------------------------- ## 
 
 class QueryHandler(Handler):
     def __init__(self, dbPathOrUrl):
@@ -140,7 +314,7 @@ class QueryHandler(Handler):
     def getById(self, id):
         pass
 
-class JournalQueryHandler(QueryHandler):
+class JournalQueryHandler(QueryHandler): #Shiho and Regina
     def __init__(self, dbPathOrUrl):
         super().__init__(dbPathOrUrl)
         self.fixed_schema_select = "PREFIX schema:<https://schema.org/> SELECT ?journal ?issn ?eissn ?title ?publisher ?language ?license ?seal ?apc"
@@ -267,5 +441,191 @@ class JournalQueryHandler(QueryHandler):
         journals_withSeal_df = get(self.dbPathOrUrl, query, True)
         return journals_withSeal_df
 
-class CategoryQueryHandler(QueryHandler):
-    pass
+class CategoryQueryHandler(QueryHandler): #Anton and Anouk
+
+    def __init__(self, dbPathOrUrl):
+        super().__init__(dbPathOrUrl)
+
+    def getById(self, id:str) -> DataFrame:
+
+        
+        # RETURN A JOIN OF ALL THE TABLES THAT HAVE THE ID OF THE JOURNAL.
+        # This is the query that will be used to get the journal with the id given.
+        conn = connect(self.dbPathOrUrl)
+        cursor = conn.cursor()
+
+        query = """
+        
+        
+        SELECT DISTINCT j.issn, j.eissn, c.category_id, c.quartile, aj.area_id 
+        FROM journals AS j
+        JOIN categories AS c ON j.journal_id = c.journal_id
+        JOIN areas_journals AS aj ON j.journal_id = aj.journal_id
+        WHERE (j.issn = ? OR j.eissn = ?);
+    
+        
+        """
+
+        cursor.execute(query, (id, id))
+
+        print("query:", query)
+
+
+        journals = cursor.fetchall()
+        df = pd.DataFrame(journals)
+
+        conn.close()
+        
+        return df
+
+        
+
+
+    def getAllCategories(self) -> DataFrame:
+
+        # return all the categories in a database, with no repetitions.
+
+        conn = connect(self.dbPathOrUrl)
+        cursor = conn.cursor()
+        cursor.execute("SELECT category_id FROM categories;")
+        
+        categories = cursor.fetchall()
+        df = pd.DataFrame(categories, columns=["category_id"])
+
+        conn.close()
+    
+        return df
+
+
+
+    def getAllAreas(self) -> DataFrame:
+
+        # return all the areas in a database, with no repetitions.
+        conn = connect(self.dbPathOrUrl)
+        cursor = conn.cursor()
+        cursor.execute("SELECT area_id FROM areas;")
+        
+        categories = cursor.fetchall()
+        df = pd.DataFrame(categories, columns=["area_id"])
+
+        conn.close()
+        
+        return df
+
+        
+
+
+    def getCategoriesWithQuartile(self, quartiles:set[str]) -> DataFrame:
+
+        if len(quartiles) == 0:
+            # If no quartile is given, we return all the categories.
+            return self.getAllCategories()
+            
+        conn = connect(self.dbPathOrUrl)
+        cursor = conn.cursor()
+
+        # WE CREATE SUCH QUERY THAT IT RETURNS WHAT IS NEEDED.
+        # For that we put in intersect as many times as the number of quartiles -1.
+
+
+        query =  f"""
+                    SELECT DISTINCT category_id 
+                    FROM categories
+                    WHERE quartile IN ({
+
+                        ', '.join(['?'] * len(quartiles)) 
+
+                    })
+                    """ # This willl create as many "?" placeholders as the number of quartiles asked for to execute in the query.
+
+        print("query:", query)
+
+        # We use said query on the database.
+        cursor.execute(query, tuple(quartiles))
+        
+        # We turn it into a Dataframe
+        categories = cursor.fetchall()
+        df = pd.DataFrame(categories, columns=["category_id"])
+
+        conn.close()
+
+        return df
+
+
+    def getCategoriesAssignedToAreas(self, area_ids:set[str] ) -> DataFrame:
+        if len(area_ids) == 0:
+            # If no area is given, we return all the categories.
+            return self.getAllCategories()
+
+        conn = connect(self.dbPathOrUrl)
+        cursor = conn.cursor()
+
+
+        query =  f"""
+
+                    SELECT DISTINCT category_id
+
+                    FROM categories AS jc
+                    JOIN journals AS j ON jc.journal_id = j.journal_id
+                    JOIN areas_journals AS aj ON j.journal_id = aj.journal_id
+
+                    WHERE aj.area_id IN ({
+
+                        ', '.join(['?'] * len(area_ids)) 
+
+                    })
+
+                    """ 
+        print(query)
+
+        # We use said query on the database.
+        cursor.execute(query, tuple(area_ids))
+
+        
+        # We turn it into a Dataframe
+        categories = cursor.fetchall()
+        df = pd.DataFrame(categories, columns=["category_id"])
+
+        conn.close()
+
+        return df
+
+
+
+    def getAreasAssignedToCategories(self, categories:set[str] ) -> DataFrame:
+
+        if len(categories) == 0:
+            # If no area is given, we return all the categories.
+            return self.getAllAreas()
+
+        conn = connect(self.dbPathOrUrl)
+        cursor = conn.cursor()
+
+
+        query =  f"""
+
+                    SELECT DISTINCT area_id
+
+                    FROM categories AS c
+                    JOIN journals AS j ON c.journal_id = j.journal_id
+                    JOIN areas_journals AS aj ON j.journal_id = aj.journal_id
+
+                    WHERE c.category_id IN ({
+
+                        ', '.join(['?'] * len(categories)) 
+
+                    })
+
+                    """ 
+
+        # We use said query on the database.
+        cursor.execute(query, tuple(categories))
+        
+        # We turn it into a Dataframe
+        areas = cursor.fetchall()
+        df = pd.DataFrame(areas, columns=["area_id"])
+
+        conn.close()
+
+        return df
+    
